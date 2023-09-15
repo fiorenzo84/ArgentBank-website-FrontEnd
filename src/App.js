5f1ceb0f8d69b689cc2379a78bb6c723412dcc64
Florent Tuppo <florent.tuppo@gmail.com>
2023-09-15 16:39:15 +0200

    test de recupération

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TestComponent from "./TestComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TestComponent />
     </div>
   );
 }
 
 export default App;
+
+// CREE L ARCHITECTURE DU PROJET
+// CREE LE ROUTING
+// CONFIGURER LE STORE REDUX
+// INTEGRATION DES PAGES
