0da432c79cb55dce7f945de969833b9976939dc6
Florent Tuppo <florent.tuppo@gmail.com>
2023-09-28 18:11:06 +0200

    modification nom du dossier

---
+++ src/api/apiUrls.js
@@ -0,0 +1,6 @@
+export const API_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:3001/api/v1/user";
+
+export const URL_LOGIN = `${API_URL}/login`;
+export const URL_SINGUP = `${API_URL}/signup`;
+export const URL_PROFILE = `${API_URL}/profile`;
