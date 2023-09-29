d58aa686a599314a10cfb62c3bb3b2e213d05f3a
Florent Tuppo <florent.tuppo@gmail.com>
2023-09-29 13:03:11 +0200

    modification

--- src/api/apiUrls.js
+++ src/api/apiUrls.js
@@ -1,5 +1,4 @@
-export const API_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:3001/api/v1/user";
+export const API_URL = process.env.REACT_APP_API_URL;
 
 export const URL_LOGIN = `${API_URL}/login`;
 export const URL_SINGUP = `${API_URL}/signup`;
